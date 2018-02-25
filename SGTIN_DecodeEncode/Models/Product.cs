b70fd7712d61e25b6c1e196c980032120ef0b07f
Genato <druidhr@gmail.com>
2018-02-25 02:50:43 +0100

    Removed references that are not used.

--- SGTIN_DecodeEncode/Models/Product.cs
+++ SGTIN_DecodeEncode/Models/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SGTIN_DecodeEncode.Models
+﻿namespace SGTIN_DecodeEncode.Models
 {
     class Product
     {
